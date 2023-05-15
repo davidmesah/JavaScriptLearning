cf57cc8331b521ae3bd1ddc2dd29d828967cce8a
David Mesa H <105566843+davidmesah@users.noreply.github.com>
2023-05-15 16:04:06 -0500

    Potencias de 2

---
+++ Potencias de 2/codigo.js
@@ -0,0 +1,13 @@
+//Función de Potencias de Dos Hasta 100
+function Potencia_De_2 ()
+{
+  var poten = 0;
+
+    for (var i = 2; i <=100; i += 2)
+    {
+        Math.pow(i, poten);
+        {
+          console.log(i);
+        }
+    }
+}
