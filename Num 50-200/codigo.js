648e9e08d87fc31ed2c8acc4b0346bfb0f09b913
David Mesa H <105566843+davidmesah@users.noreply.github.com>
2023-05-15 16:03:48 -0500

    Num 50-200

---
+++ Num 50-200/codigo.js
@@ -0,0 +1,11 @@
+//Función de Numeros entre 50 y 200
+function Num_Entre_50_200 ()
+{
+  for (var i = 50; i <=200; i += 2)
+    {
+        var acu = 0;
+        console.log(i);
+        acu+=i;
+    }
+ 
+}
